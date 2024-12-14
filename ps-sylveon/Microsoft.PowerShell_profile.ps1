da03150c5f57a1c035a0c96171b7cdfd02c94964
orayanics <nicole.oraya.cics@ust.edu.ph>
2024-12-14 13:48:17 +0800

    ohmyposh to starship

--- ps-sylveon/Microsoft.PowerShell_profile.ps1
+++ ps-sylveon/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,8 @@
+Import-Module -Name Terminal-Icons
+Import-Module -Name PSReadLine
+
 # Define the path to the .cow file
-$cowFile = "C:\Users\nikolai\Documents\PowerShell\sylveon.txt"
+$cowFile = "C:\shell-profiles\ps-sylveon\sylveon.txt"
 
 # Read the .cow file
 $cowContent = Get-Content $cowFile -Raw
@@ -7,4 +10,27 @@ $cowContent = Get-Content $cowFile -Raw
 # Display the content as-is
 Write-Host $cowContent -NoNewline
 
-oh-my-posh init pwsh --config 'D:\Profiles\sylveon\catppuccin_mocha.omp.json' | Invoke-Expression
+# Get the current date and user
+$currentDate = Get-Date -Format "yyyy-MM-dd"
+$currentUser = [Environment]::UserName
+
+
+# Create the string with spaces and text
+$alignedText = ("Today is " + "$currentDate")
+$userName = ("Welcome, " + "$currentUser" + "!")
+
+# Write the new content to the console
+Write-Host $alignedText
+Write-Host $userName
+
+# ssr transient
+function Invoke-Starship-TransientFunction {
+  &starship module character
+}
+
+# starship completion
+. C:\Users\nikolai\Documents\PowerShell\ssr_completion.ps1
+
+# starship
+Invoke-Expression (& 'C:\Program Files\starship\bin\starship.exe' init powershell --print-full-init | Out-String)
+Enable-TransientPrompt
